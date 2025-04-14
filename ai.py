bd5692b59d8840e54d5a9e7a0e48427ac1881075
ashinth-ffyo <74600376+ashinth-ffyo@users.noreply.github.com>
2025-04-14 11:30:59 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,24 +1,6 @@
 import joblib
 from time import sleep
 import pandas as pd
-import gdown
-import joblib
-
-# Direct download link (replace FILE_ID)
-
-gdrive_url1 = "https://drive.google.com/uc?id=1bNEKUTLz7DQZkigPzJKpJLhJreLlTx7-"  
-model_path1 = "feature_encoders.joblib"
-
-gdrive_url2 = "https://drive.google.com/uc?id=1ZO-WYJxD8AySv3Qxc80szBMn7KlsTvR8"  
-model_path2 = "label_encoder_y.joblib"
-
-gdrive_url = "https://drive.google.com/uc?id=1E0-9bbKUwcdyWL4cxwGYkIkUfUtBeut8"  
-model_path = "disease_predictor.joblib"
-
-# Download (only if not already present)
-gdown.download(gdrive_url, model_path, quiet=False)
-gdown.download(gdrive_url1, model_path1, quiet=False)
-gdown.download(gdrive_url2, model_path2, quiet=False)
 
 class DiseasePredictor:
     def __init__(self):
